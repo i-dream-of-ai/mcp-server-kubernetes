c98bc6f06880b187b5dde13e1c6b54bfcb701276
Suyog Sonwalkar <flux159@gmail.com>
2025-03-23 20:33:22 -0700

    Adding sse transport under env var
    Summary:
    
    Test Plan:

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,8 @@ import { serverConfig } from "./config/server-config.js";
 import { createDeploymentSchema } from "./config/deployment-config.js";
 import { listNamespacesSchema } from "./config/namespace-config.js";
 import { cleanupSchema } from "./config/cleanup-config.js";
+import express from "express";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
 
 const k8sManager = new KubernetesManager();
 
@@ -287,8 +289,42 @@ server.setRequestHandler(
   resourceHandlers.readResource
 );
 
-const transport = new StdioServerTransport();
-await server.connect(transport);
+if (process.env.ENABLE_UNSAFE_SSE_TRANSPORT) {
+  const app = express();
+
+  // Currently just copying from docs & allowing for multiple transport connections: https://modelcontextprotocol.io/docs/concepts/transports#server-sent-events-sse
+  // TODO: If exposed to web, then this will enable any client to connect to the server via http - so marked as UNSAFE until mcp has a proper auth solution.
+  let transports: Array<SSEServerTransport> = [];
+
+  app.get("/sse", async (req, res) => {
+    const transport = new SSEServerTransport("/messages", res);
+    transports.push(transport);
+    await server.connect(transport);
+  });
+
+  app.post("/messages", (req, res) => {
+    const transport = transports.find(
+      (t) => t.sessionId === req.query.sessionId
+    );
+
+    if (transport) {
+      transport.handlePostMessage(req, res);
+    } else {
+      res
+        .status(404)
+        .send("Not found. Must pass valid sessionId as query param.");
+    }
+  });
+
+  const port = process.env.PORT || 3000;
+  app.listen(port);
+  console.log(
+    `mcp-kubernetes-server is listening on port ${port}\nUse the following url to connect to the server:\n\http://localhost:${port}/sse`
+  );
+} else {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
 
 ["SIGINT", "SIGTERM"].forEach((signal) => {
   process.on(signal, async () => {
